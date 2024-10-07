ab2bb4905c2090107035f2c483d7814a57c4138b
AstronGaming <yashudkl@gmail.com>
2024-10-07 23:15:34 +0545

    Header section added

---
+++ src/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+import './Header.css';
+function Header(){
+    return (
+        <div className="Header">
+            <h1>Hello this is header section for upamanyu academy</h1>
+            <p>React is awesome!</p>
+        </div>
+    );
+}
+export default Header;
