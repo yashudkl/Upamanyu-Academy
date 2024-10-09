71e2f7ad5c39f9d67d29559bd76dab82941c6060
AstronGaming <yashudkl@gmail.com>
2024-10-09 09:10:24 +0545

    Navbar-Updated

--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,27 @@
 import React from "react";
-import './Header.css';
-function Header(){
-    return (
-        <div className="Header">
-            <h1>Hello this is header section for upamanyu academy</h1>
-            <p>React is awesome!</p>
-        </div>
-    );
+import logo from './images/logo.png';
+import "./Header.css";
+function Header() {
+  return (
+    <div className="Header">
+        <header>
+        <img src="" alt="Logo" />
+        <nav class="navbar">
+  <ul class="nav-links">
+    <li><a href="#home">Home</a></li>
+    <li><a href="#Lectures">Lectures</a></li>
+    <li><a href="#Live">Live</a></li>
+    <li><a href="#Questions">Questions</a></li>
+    <li><a href="#About">About</a></li>
+    <li><a href="#Contact">Contact</a></li>
+    
+  </ul>
+</nav>
+        </header>
+
+
+
+    </div>
+  );
 }
 export default Header;
