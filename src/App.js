ae883fe7e9566ed474eb0f2e3dab0b371d2c2eff
AstronGaming <yashudkl@gmail.com>
2024-10-12 23:58:00 +0545

    font arranged

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
 import Header from './Header';
+import Intro from './Intro';
 
 function App() {
   return (
     <div className="App">
 <Header/>
+<Intro/>
     </div>
   );
 }
