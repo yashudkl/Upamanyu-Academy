4cfa96d15ee2b9dedd8bbc08ecb0ab65ca24c4b8
AstronGaming <yashudkl@gmail.com>
2024-10-12 23:56:49 +0545

    image and content

--- src/Intro.js
+++ src/Intro.js
@@ -1,10 +1,22 @@
 import React from "react";
+import logo from './images/intro-image.png';
+import './Intro.css';
 function Intro(){
  return( 
     <div className="Intro">
+<div class="intro-card">
+ <img src={logo} alt="Upamanyu Academy Logo" className="intro-image" />
+ <div className="intro-content">
+        <h1>Welcome to Upamanyu Academy!</h1>
         <p>
+          At Upamanyu Academy, we believe in empowering students to achieve their full potential through innovative and effective learning experiences...
         </p>
-
+        <p>
+          Join us at Upamanyu Academy, where we are dedicated to "Making Education Better in Nepal." Together, we can build a brighter future for our students and our nation.
+        </p>
+      </div>
+</div>
     </div>
  );
 }
+export default Intro;
