5e2dad22fd60e8b1596463fc7aa3d35d21930e4f
AstronGaming <yashudkl@gmail.com>
2024-10-14 15:21:49 +0545

    Drop-down added

--- src/Header.js
+++ src/Header.js
@@ -1,27 +1,48 @@
 import React from "react";
-import logo from './images/logo.png';
 import "./Header.css";
+
 function Header() {
   return (
     <div className="Header">
-        <header>
-        <img src="" alt="Logo" />
-        <nav class="navbar">
-  <ul class="nav-links">
-    <li><a href="#home">Home</a></li>
-    <li><a href="#Lectures">Lectures</a></li>
-    <li><a href="#Live">Live</a></li>
-    <li><a href="#Questions">Questions</a></li>
-    <li><a href="#About">About</a></li>
-    <li><a href="#Contact">Contact</a></li>
-    
-  </ul>
-</nav>
-        </header>
-
-
-
+      <header>
+        <nav className="navbar">
+          <ul className="nav-links">
+            <li>
+              <a href="#home">Home</a>
+            </li>
+            <li className="nav-item dropdown">
+              <a href="#Lectures">Lectures</a>
+              <ul className="dropdown-menu">
+                <li>Grade 10</li>
+                <li>Grade 11</li>
+                <li>Grade 12</li>
+                <li>Entrance</li>
+              </ul>
+            </li>
+            <li>
+              <a href="#Live">Live</a>
+            </li>
+            <li className="nav-item dropdown">
+              <a href="#Questions">Questions</a>
+              <ul className="dropdown-menu">
+                <li>Grade 10</li>
+                <li>Grade 11</li>
+                <li>Grade 12</li>
+                <li>Entrance</li>
+              </ul>
+            </li>
+            <li>
+              <a href="#About">About</a>
+            </li>
+            <li>
+              <a href="#Contact">Contact</a>
+            </li>
+          </ul>
+        </nav>
+        <button className="button">Sign in</button>
+      </header>
     </div>
   );
 }
+
 export default Header;
